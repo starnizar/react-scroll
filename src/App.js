0036d75bba31632cd30aa02bdce630938146031b
starnizar <nizarincorp@gmail.com>
2021-10-21 20:21:40 +0300

    --

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id='red' className='red'>
+          <a href='#blue'>To blue</a>
+          <a href='#green'>To blue</a>
+      </div>
+      <div id='blue' className='blue'>
+          <a href='#red'>To blue</a>
+          <a href='#green'>To blue</a>
+      </div>
+      <div id='green' className='green'>
+        <a href='#red'>To blue</a>
+          <a href='#green'>To blue</a>
+      </div>
     </div>
   );
 }
