9d273e08cc0652dd1c3f56ead8f59c840db211de
starnizar <nizarincorp@gmail.com>
2021-10-21 20:30:55 +0300

    -

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ function App() {
     <div className="App">
       <div id='red' className='red'>
           <a href='#blue'>To blue</a>
-          <a href='#green'>To blue</a>
+          <a href='#green'>To green</a>
       </div>
       <div id='blue' className='blue'>
-          <a href='#red'>To blue</a>
-          <a href='#green'>To blue</a>
+          <a href='#red'>To red</a>
+          <a href='#blue'>To blue</a>
       </div>
       <div id='green' className='green'>
-        <a href='#red'>To blue</a>
-          <a href='#green'>To blue</a>
+        <a href='#red'>To red</a>
+          <a href='#green'>To green</a>
       </div>
     </div>
   );
